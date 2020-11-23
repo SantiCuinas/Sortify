ffe01e1ad43b2f5aeffe694739004b94064baa03
SantiagoCuinas <santiago.cuinas@southworks.com>
2020-11-22 22:44:51 -0300

    🛂 Add Spotify auth

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import App from "./App";
+import Main from "./Main";
+
+export default function Routes() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/main/:access_token?" component={Main} />
+        <Route path="/" component={App} />
+      </Switch>
+    </Router>
+  );
+}
